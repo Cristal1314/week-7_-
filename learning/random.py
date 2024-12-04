2f03097b2453c404f951640d7030881122e8ad2b
Jimenez <CFJimenez@cps.k12.il.us>
2024-12-04 08:32:51 -0600

    https://github.com/Cristal1314/week-7_-/blob/main/learning/random.py

--- learning/random.py
+++ learning/random.py
@@ -31,4 +31,6 @@ print(random_numbers)
 # Random Practice #3
 # Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
 
-# names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+raffle = random.choice(names)
+print(raffle)
