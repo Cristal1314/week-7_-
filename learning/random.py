8088d90ba0e03b00673a0aaec0c84b560e046c91
Jimenez <CFJimenez@cps.k12.il.us>
2024-12-04 08:31:53 -0600

    https://github.com/Cristal1314/week-7_-

--- learning/random.py
+++ learning/random.py
@@ -1,12 +1,32 @@
+import random
+
+# x = random.randint(1,6)
+# print(x)
+
+# y = random.random()
+# print(y)
+
+# myList = ['rock', 'paper', 'scissors']
+# z = random.choice(myList)
+# print(z)
+
+# cards = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
+# random.shuffle(cards)
+
+# print(cards)
+
+
 
 ################################################random in python#################################################
 # Random Practice #1
 # Implement the randint() function from the random library that allows you to obtain an integer from 1 to 10, and store that value in a variable called random_number.
-
+random_number = random.randint(1,10)
+print(random_number)
 
 # Random Practice #2
 # Implement the random() function from the random library to obtain a real number between 0 and 1, and store that value in a variable called random_number.
-
+random_numbers = random.random(0,1)
+print(random_numbers)
 
 # Random Practice #3
 # Use the random library's choice() method to get a random item from the list of names below, and store the chosen name in a variable called raffle.
